fadfe9563ea30da2b6d7764b1b8c5fc9eb493c1f
Fito <a.menendezrua@gmail.com>
2024-04-03 05:26:48 +0200

    Notes 3 Done

---
+++ Python/Chapter10/energy.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = 1
+Z = 1
+m = 9.1093837e-31
+e = 1.60217662e-19
+ep = 8.854187817e-12
+hbar = 1.054571817e-34
+
+
+def E(n):
+    return (-Z**2 * e**4 * m) / (32 * np.pi**2 * ep**2 * hbar**2 * n**2) +
+
+n_values = np.arange(1, 11)
+
+energy_values = [E(n) for n in n_values]
+
+plt.plot(n_values, energy_values, marker='o')
+plt.xlabel('n')
+plt.ylabel('Energy (J)')
+plt.title('Energy vs. Principal Quantum Number (n)')
+plt.grid(True)
+plt.savefig('images/energy_vs_n.png')
